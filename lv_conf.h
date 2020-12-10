319d2a373319c042113f7b18f00aa4eead2f47dd
Ken Stanley <dohpaz@gmail.com>
2020-12-09 22:37:11 -0500

    Add Roboto Light, Regular, and Black fonts

--- lv_conf.h
+++ lv_conf.h
@@ -417,12 +417,38 @@ typedef void * lv_indev_drv_user_data_t;            /*Type of user data in the i
  * #define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(my_font_1) \
  *                                LV_FONT_DECLARE(my_font_2)
  */
-#define LV_FONT_CUSTOM_DECLARE
+#define LV_FONT_CUSTOM_DECLARE  LV_FONT_DECLARE(roboto_regular_10) \
+                                LV_FONT_DECLARE(roboto_regular_12) \
+                                LV_FONT_DECLARE(roboto_regular_14) \
+                                LV_FONT_DECLARE(roboto_regular_16) \
+                                LV_FONT_DECLARE(roboto_regular_18) \
+                                LV_FONT_DECLARE(roboto_regular_24) \
+                                LV_FONT_DECLARE(roboto_regular_48) \
+                                LV_FONT_DECLARE(roboto_regular_96) \
+                                LV_FONT_DECLARE(roboto_regular_120) \
+                                LV_FONT_DECLARE(roboto_light_10) \
+                                LV_FONT_DECLARE(roboto_light_12) \
+                                LV_FONT_DECLARE(roboto_light_14) \
+                                LV_FONT_DECLARE(roboto_light_16) \
+                                LV_FONT_DECLARE(roboto_light_18) \
+                                LV_FONT_DECLARE(roboto_light_24) \
+                                LV_FONT_DECLARE(roboto_light_48) \
+                                LV_FONT_DECLARE(roboto_light_96) \
+                                LV_FONT_DECLARE(roboto_light_120) \
+                                LV_FONT_DECLARE(roboto_black_10) \
+                                LV_FONT_DECLARE(roboto_black_12) \
+                                LV_FONT_DECLARE(roboto_black_14) \
+                                LV_FONT_DECLARE(roboto_black_16) \
+                                LV_FONT_DECLARE(roboto_black_18) \
+                                LV_FONT_DECLARE(roboto_black_24) \
+                                LV_FONT_DECLARE(roboto_black_48) \
+                                LV_FONT_DECLARE(roboto_black_96) \
+                                LV_FONT_DECLARE(roboto_black_120)
 
 /* Enable it if you have fonts with a lot of characters.
  * The limit depends on the font size, font face and bpp
  * but with > 10,000 characters if you see issues probably you need to enable it.*/
-#define LV_FONT_FMT_TXT_LARGE   0
+#define LV_FONT_FMT_TXT_LARGE 1
 
 /* Enables/disables support for compressed fonts. If it's disabled, compressed
  * glyphs cannot be processed by the library and won't be rendered.
