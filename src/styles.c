d5ae49f5303b49026682d1f93bc2589b797ab730
Ken Stanley <dohpaz@gmail.com>
2020-12-16 21:56:30 -0500

    Rename the styles using a naming standard
    1. All styles are prefixed with ‘style_’
    2. Second part is the style (e.g., background, padding, etc)
    3. Remaining parts describe the change (e.g., none, transparent_full, etc)

--- src/styles.c
+++ src/styles.c
@@ -1,18 +1,18 @@
 #include "styles.h"
 
-lv_style_t transBackground;
-lv_style_t halfTransBackground;
-lv_style_t padding;
-lv_style_t nPadding;
-lv_style_t blkText;
-lv_style_t wBorder;
-lv_style_t nBorder;
-lv_style_t triBorderRight;
-lv_style_t blkBackground;
-lv_style_t bluBackground;
-lv_style_t mdDayFont;
-lv_style_t lgDayFont;
-lv_style_t bgBlendMode;
+lv_style_t style_background_transparent_full;
+lv_style_t style_background_transparent_half;
+lv_style_t style_padding_default;
+lv_style_t style_padding_none;
+lv_style_t style_text_color_black;
+lv_style_t style_border_color_white;
+lv_style_t style_border_none;
+lv_style_t style_border_sides_right_none;
+lv_style_t style_background_color_black;
+lv_style_t style_background_color_blue;
+lv_style_t style_font_black_medium;
+lv_style_t style_font_black_large;
+lv_style_t style_background_overlay_color_black;
 
 void styles_init(void) {
     static bool initialized = false;
@@ -22,50 +22,50 @@ void styles_init(void) {
         return;
     }
 
-    lv_style_init(&transBackground);
-    lv_style_set_bg_opa(&transBackground, LV_STATE_DEFAULT, LV_OPA_TRANSP);
+    lv_style_init(&style_background_transparent_full);
+    lv_style_set_bg_opa(&style_background_transparent_full, LV_STATE_DEFAULT, LV_OPA_TRANSP);
 
-    lv_style_init(&halfTransBackground);
-    lv_style_set_bg_opa(&halfTransBackground, LV_STATE_DEFAULT, LV_OPA_50);
+    lv_style_init(&style_background_transparent_half);
+    lv_style_set_bg_opa(&style_background_transparent_half, LV_STATE_DEFAULT, LV_OPA_50);
 
-    lv_style_init(&padding);
-    lv_style_set_pad_all(&padding, LV_STATE_DEFAULT, DEFAULT_PADDING);
+    lv_style_init(&style_padding_default);
+    lv_style_set_pad_all(&style_padding_default, LV_STATE_DEFAULT, DEFAULT_PADDING);
 
-    lv_style_init(&nPadding);
-    lv_style_set_pad_all(&nPadding, LV_STATE_DEFAULT, 0);
+    lv_style_init(&style_padding_none);
+    lv_style_set_pad_all(&style_padding_none, LV_STATE_DEFAULT, 0);
 
-    lv_style_init(&blkText);
-    lv_style_set_text_color(&blkText, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+    lv_style_init(&style_text_color_black);
+    lv_style_set_text_color(&style_text_color_black, LV_STATE_DEFAULT, LV_COLOR_BLACK);
 
-    lv_style_init(&wBorder);
-    lv_style_set_border_color(&wBorder, LV_STATE_DEFAULT, LV_COLOR_WHITE);
-    lv_style_set_border_width(&wBorder, LV_STATE_DEFAULT, DEFAULT_BORDER);
+    lv_style_init(&style_border_color_white);
+    lv_style_set_border_color(&style_border_color_white, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_style_set_border_width(&style_border_color_white, LV_STATE_DEFAULT, DEFAULT_BORDER);
 
-    lv_style_init(&nBorder);
-    lv_style_set_border_width(&nBorder, LV_STATE_DEFAULT, 0);
+    lv_style_init(&style_border_none);
+    lv_style_set_border_width(&style_border_none, LV_STATE_DEFAULT, 0);
 
-    lv_style_init(&triBorderRight);
-    lv_style_set_border_side(&triBorderRight, LV_STATE_DEFAULT, LV_BORDER_SIDE_LEFT | LV_BORDER_SIDE_BOTTOM | LV_BORDER_SIDE_TOP);
+    lv_style_init(&style_border_sides_right_none);
+    lv_style_set_border_side(&style_border_sides_right_none, LV_STATE_DEFAULT, LV_BORDER_SIDE_LEFT | LV_BORDER_SIDE_BOTTOM | LV_BORDER_SIDE_TOP);
 
-    lv_style_init(&blkBackground);
-    lv_style_set_bg_color(&blkBackground, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+    lv_style_init(&style_background_color_black);
+    lv_style_set_bg_color(&style_background_color_black, LV_STATE_DEFAULT, LV_COLOR_BLACK);
 
-    lv_style_init(&bluBackground);
-    lv_style_set_bg_color(&bluBackground, LV_STATE_DEFAULT, LV_COLOR_BLUE);
+    lv_style_init(&style_background_color_blue);
+    lv_style_set_bg_color(&style_background_color_blue, LV_STATE_DEFAULT, LV_COLOR_BLUE);
 
-    lv_style_init(&mdDayFont);
-    lv_style_set_text_font(&mdDayFont, LV_STATE_DEFAULT, &roboto_black_24);
-    lv_style_set_value_align(&mdDayFont, LV_STATE_DEFAULT, LV_ALIGN_IN_TOP_MID);
-    lv_style_set_value_font(&mdDayFont, LV_STATE_DEFAULT, &roboto_black_24);
+    lv_style_init(&style_font_black_medium);
+    lv_style_set_text_font(&style_font_black_medium, LV_STATE_DEFAULT, &roboto_black_24);
+    lv_style_set_value_align(&style_font_black_medium, LV_STATE_DEFAULT, LV_ALIGN_IN_TOP_MID);
+    lv_style_set_value_font(&style_font_black_medium, LV_STATE_DEFAULT, &roboto_black_24);
 
-    lv_style_init(&lgDayFont);
-    lv_style_set_text_font(&lgDayFont, LV_STATE_DEFAULT, &roboto_black_96);
-    lv_style_set_value_align(&lgDayFont, LV_STATE_DEFAULT, LV_ALIGN_CENTER);
-    lv_style_set_value_font(&lgDayFont, LV_STATE_DEFAULT, &roboto_black_96);
+    lv_style_init(&style_font_black_large);
+    lv_style_set_text_font(&style_font_black_large, LV_STATE_DEFAULT, &roboto_black_96);
+    lv_style_set_value_align(&style_font_black_large, LV_STATE_DEFAULT, LV_ALIGN_CENTER);
+    lv_style_set_value_font(&style_font_black_large, LV_STATE_DEFAULT, &roboto_black_96);
 
-    lv_style_init(&bgBlendMode);
-    lv_style_copy(&bgBlendMode, &blkBackground);
-    lv_style_copy(&bgBlendMode, &halfTransBackground);
+    lv_style_init(&style_background_overlay_color_black);
+    lv_style_copy(&style_background_overlay_color_black, &style_background_color_black);
+    lv_style_copy(&style_background_overlay_color_black, &style_background_transparent_half);
 
     initialized = true;
     LV_LOG_INFO("styles_init ready");
--- src/styles.h
+++ src/styles.h
@@ -6,19 +6,19 @@
 #define DEFAULT_PADDING 16
 #define DEFAULT_BORDER 5
 
-extern lv_style_t transBackground;
-extern lv_style_t halfTransBackground;
-extern lv_style_t padding;
-extern lv_style_t nPadding;
-extern lv_style_t blkText;
-extern lv_style_t wBorder;
-extern lv_style_t nBorder;
-extern lv_style_t triBorderRight;
-extern lv_style_t blkBackground;
-extern lv_style_t bluBackground;
-extern lv_style_t mdDayFont;
-extern lv_style_t lgDayFont;
-extern lv_style_t bgBlendMode;
+extern lv_style_t style_background_color_black;
+extern lv_style_t style_background_color_blue;
+extern lv_style_t style_background_overlay_color_black;
+extern lv_style_t style_background_transparent_full;
+extern lv_style_t style_background_transparent_half;
+extern lv_style_t style_border_color_white;
+extern lv_style_t style_border_none;
+extern lv_style_t style_border_sides_right_none;
+extern lv_style_t style_font_black_large;
+extern lv_style_t style_font_black_medium;
+extern lv_style_t style_padding_default;
+extern lv_style_t style_padding_none;
+extern lv_style_t style_text_color_black;
 
 void styles_init(void);
 
