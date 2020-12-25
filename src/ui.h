4241f53d0eaeec3cee08e2b556de14603698c3c7
Ken Stanley <dohpaz@gmail.com>
2020-12-25 15:36:48 -0500

    Simplify the structs

--- src/ui.h
+++ src/ui.h
@@ -4,29 +4,7 @@
 #include "lvgl/lvgl.h"
 #include "configuration.h"
 
-struct ui_bottom_left_page_t;
-struct ui_left_page_t;
-struct ui_page_t;
-struct ui_right_page_t;
-struct ui_t;
-struct ui_settings_t;
-struct ui_settings_general_t;
-struct ui_settings_calendars_t;
-struct ui_top_left_page_t;
-struct ui_top_page_t;
-
-typedef struct ui_bottom_left_page_t ui_bottom_left_page_t;
-typedef struct ui_left_page_t ui_left_page_t;
-typedef struct ui_page_t ui_page_t;
-typedef struct ui_right_page_t ui_right_page_t;
-typedef struct ui_t ui_t;
-typedef struct ui_settings_t ui_settings_t;
-typedef struct ui_settings_general_t ui_settings_general_t;
-typedef struct ui_settings_calendars_t ui_settings_calendars_t;
-typedef struct ui_top_left_page_t ui_top_left_page_t;
-typedef struct ui_top_page_t ui_top_page_t;
-
-struct ui_top_left_page_t {
+typedef struct ui_top_left_page_t {
     lv_obj_t * main;
     lv_obj_t * container;
 
@@ -37,15 +15,15 @@ struct ui_top_left_page_t {
     lv_obj_t * label_day;
     lv_obj_t * label_month;
     lv_obj_t * label_year;
-};
+} ui_top_left_page_t;
 
-struct ui_bottom_left_page_t {
+typedef struct ui_bottom_left_page_t {
     lv_obj_t * main;
 
     lv_obj_t * itinerary;
-};
+} ui_bottom_left_page_t;
 
-struct ui_top_page_t {
+typedef struct ui_top_page_t {
     lv_obj_t * main;
     lv_obj_t * container;
 
@@ -54,31 +32,31 @@ struct ui_top_page_t {
 
     lv_obj_t * label_time;
     lv_obj_t * btn_action_settings;
-};
+} ui_top_page_t;
 
-struct ui_left_page_t {
+typedef struct ui_left_page_t {
     lv_obj_t * main;
 
     ui_top_left_page_t top;
     ui_bottom_left_page_t bottom;
-};
+} ui_left_page_t;
 
-struct ui_right_page_t {
+typedef struct ui_right_page_t {
     lv_obj_t * main;
     lv_obj_t * container;
 
     lv_obj_t * calendar;
-};
+} ui_right_page_t;
 
-struct ui_settings_general_t {
+typedef struct ui_settings_general_t {
     lv_obj_t * main;
-};
+} ui_settings_general_t;
 
-struct ui_settings_calendars_t {
+typedef struct ui_settings_calendars_t {
     lv_obj_t * main;
-};
+} ui_settings_calendars_t;
 
-struct ui_settings_t {
+typedef struct ui_settings_t {
     lv_obj_t * main;
     lv_obj_t * list;
 
@@ -89,17 +67,17 @@ struct ui_settings_t {
         ui_settings_general_t general;
         ui_settings_calendars_t calendars;
     };
-};
+} ui_settings_t;
 
-struct ui_page_t {
+typedef struct ui_page_t {
     lv_obj_t * main;
 
     ui_top_page_t top;
     ui_left_page_t left;
     ui_right_page_t right;
-};
+} ui_page_t;
 
-struct ui_t {
+typedef struct ui_t {
     lv_disp_t * display;
     lv_obj_t * screen;
 
@@ -108,7 +86,7 @@ struct ui_t {
     ui_settings_t settings;
 
     lv_calendar_date_t date;
-};
+} ui_t;
 
 ui_t * ui_init(void);
 
