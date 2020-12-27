5a953caffeb96bb762f3b655723c96a576b0a3f8
Ken Stanley <dohpaz@gmail.com>
2020-12-27 13:14:45 -0500

    Add a comparator for configuration

--- src/configuration.h
+++ src/configuration.h
@@ -14,4 +14,32 @@ typedef struct configuration {
 
 configuration * p_config;
 
+static inline int compareTo(const configuration lhs, const configuration rhs) {
+    if (lhs.time_flash < rhs.time_flash) {
+        return -1;
+    } else if (lhs.time_flash > rhs.time_flash) {
+        return 1;
+    }
+
+    if (lhs.time_format24 < rhs.time_format24) {
+        return -1;
+    } else if (lhs.time_format24 > rhs.time_format24) {
+        return 1;
+    }
+
+    if (lhs.time_meridiem < rhs.time_meridiem) {
+        return -1;
+    } else if (lhs.time_meridiem > rhs.time_meridiem) {
+        return 1;
+    }
+
+    if (lhs.time_screensaver < rhs.time_screensaver) {
+        return -1;
+    } else if (lhs.time_screensaver > rhs.time_screensaver) {
+        return 1;
+    }
+
+    return 0;
+}
+
 #endif //BASIC_ICS_CONFIGURATION_H
