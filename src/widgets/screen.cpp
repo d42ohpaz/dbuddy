df3828cf95627de81beb17749b1140848d4cda29
Ken Stanley <dohpaz@gmail.com>
2021-01-26 21:42:58 -0500

    Remove extraneous blank line

--- src/widgets/screen.cpp
+++ src/widgets/screen.cpp
@@ -8,7 +8,6 @@ Widgets::Screen::Screen(lv_obj_t * parent, lv_obj_t * copy) : Widget(parent, cop
 }
 
 void Widgets::Screen::init(Fonts * fonts, Styles * styles) {
-
     add_style(LV_OBJ_PART_MAIN, &styles->style_default_background_color_blue);
     add_style(LV_OBJ_PART_MAIN, &styles->style_default_background_transparent_cover);
 }
