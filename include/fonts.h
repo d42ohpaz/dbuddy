2ead2bd712d6683a4ea6b670f3531dffcb42f6be
Ken Stanley <dohpaz@gmail.com>
2021-01-26 21:11:57 -0500

    Fonts by value

--- include/fonts.h
+++ include/fonts.h
@@ -10,11 +10,11 @@ namespace DBuddy {
         Fonts(Fonts &other) = delete;
         void operator=(const Fonts &) = delete;
 
-        lv_style_t * style_default_font_black_medium{};
-        lv_style_t * style_default_font_black_small{};
-        lv_style_t * style_default_font_black_xxlarge{};
-        lv_style_t * style_default_font_regular_small{};
-        lv_style_t * style_default_font_regular_xsmall{};
+        lv_style_t style_default_font_black_medium{};
+        lv_style_t style_default_font_black_small{};
+        lv_style_t style_default_font_black_xxlarge{};
+        lv_style_t style_default_font_regular_small{};
+        lv_style_t style_default_font_regular_xsmall{};
     };
 }
 #endif // BASIC_ICS_FONTS_H
