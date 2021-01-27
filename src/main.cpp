8933a2e0b6823e6dab81776e4b9947b26a0af184
Ken Stanley <dohpaz@gmail.com>
2021-01-26 21:43:25 -0500

    Add memory logging based on environment

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,43 @@
 
 #define USE_DOUBLE_BUFFER (false)
 
+static void memory_monitor(lv_task_t *param);
+
 void setup() {
+#ifndef USE_MONITOR
+    Serial.begin(115200);
+#endif
+
     DBuddy::DBuddy::setup(
         new DBuddy::NodeMCU32s(new Adafruit_RA8875(RA8875_CS, RA8875_RESET), RA8875_INT),
         new DBuddy::Ui,
         USE_DOUBLE_BUFFER
     );
+
+    lv_task_create(memory_monitor, 5000, LV_TASK_PRIO_MID, nullptr);
 }
 
 void loop() {
     DBuddy::DBuddy::loop();
+}
+
+/**
+ * Print the memory usage periodically
+ * @param param
+ */
+static void memory_monitor(lv_task_t *param) {
+  (void)param; /*Unused*/
+
+  lv_mem_monitor_t mon;
+  lv_mem_monitor(&mon);
+
+#ifndef USE_MONITOR
+  Serial.printf("used: %6d (%3d %%), frag: %3d %%, biggest free: %6d\n",
+         (int)mon.total_size - mon.free_size, mon.used_pct, mon.frag_pct,
+         (int)mon.free_biggest_size);
+#else
+  printf("used: %6d (%3d %%), frag: %3d %%, biggest free: %6d\n",
+         (int)mon.total_size - mon.free_size, mon.used_pct, mon.frag_pct,
+         (int)mon.free_biggest_size);
+#endif
 }
