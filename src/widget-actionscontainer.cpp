59b9e37fca20c21c8f85dde4a58c1b17772f7c57
Ken Stanley <dohpaz@gmail.com>
2021-01-31 22:09:23 -0500

    Use the time container for dimensions

--- src/widget-actionscontainer.cpp
+++ src/widget-actionscontainer.cpp
@@ -4,10 +4,11 @@ using namespace dbuddy;
 
 void ActionsContainer::init() {
     lv_obj_t * menu = get_ui()->get_widget(WIDGET_MENU)->get_self();
+    lv_obj_t * time_container = get_ui()->get_widget(WIDGET_TIME_CONTAINER)->get_self();
     set_self(lv_obj_create(menu, nullptr));
 
-    set_size(lv_obj_get_width(menu) / 2, lv_obj_get_height(menu));
-    set_pos(lv_obj_get_width(menu) / 2, 0);
+    set_size(lv_obj_get_width(time_container), lv_obj_get_height(time_container));
+    set_pos(lv_obj_get_width(time_container), 0);
 
     lv_obj_add_style(get_self(), LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_border_none(LV_STATE_DEFAULT));
     lv_obj_add_style(get_self(), LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_radius_none(LV_STATE_DEFAULT));
