8faf11ff5161df9d8a04c2926cff4055900a46e4
Ken Stanley <dohpaz@gmail.com>
2021-01-31 22:15:43 -0500

    Use add_style helper

--- src/widget-actionscontainer.cpp
+++ src/widget-actionscontainer.cpp
@@ -10,7 +10,7 @@ void ActionsContainer::init() {
     set_size(lv_obj_get_width(time_container), lv_obj_get_height(time_container));
     set_pos(lv_obj_get_width(time_container), 0);
 
-    lv_obj_add_style(get_self(), LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_border_none(LV_STATE_DEFAULT));
-    lv_obj_add_style(get_self(), LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_radius_none(LV_STATE_DEFAULT));
-    lv_obj_add_style(get_self(), LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_background_transparent_full(LV_STATE_DEFAULT));
+    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_border_none(LV_STATE_DEFAULT));
+    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_radius_none(LV_STATE_DEFAULT));
+    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_background_transparent_full(LV_STATE_DEFAULT));
 }
