5a4be4450c66ff41ea8ab005823bdaf21cd3cb0b
Ken Stanley <dohpaz@gmail.com>
2021-02-02 20:22:08 -0500

    Remove unnecessary styling

--- src/widget-menu.cpp
+++ src/widget-menu.cpp
@@ -8,11 +8,6 @@ void Menu::init() {
 
     set_size(page->get_width(), DEFAULT_MENU_HEIGHT + DEFAULT_PADDING);
 
-    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_radius_none(LV_STATE_DEFAULT));
-    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_background_overlay(LV_STATE_DEFAULT));
-    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_padding_default(LV_STATE_DEFAULT));
-    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_radius_none(LV_STATE_DEFAULT));
-    add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_background_transparent_full(LV_STATE_DEFAULT));
     add_style(LV_PAGE_PART_BG, get_ui()->get_styles()->get_border_color_white(LV_STATE_DEFAULT));
     add_style(LV_PAGE_PART_BG, get_ui()->get_styles()->get_border_color_white(LV_STATE_FOCUSED));
     add_style(LV_OBJ_PART_MAIN, get_ui()->get_styles()->get_border_sides_only_bottom(LV_STATE_DEFAULT));
