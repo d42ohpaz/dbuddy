cd52a4646ac20be208de580abf9634ad8740438a
Ken Stanley <dohpaz@gmail.com>
2021-02-05 23:21:52 -0500

    Add the ability to create/delete tasks in Ui

--- include/ui.h
+++ include/ui.h
@@ -2,6 +2,7 @@
 #define DBUDDY_UI_H
 
 #include <lvgl.h>
+#include <vector>
 
 #include "widget.h"
 #include "fonts.h"
@@ -11,8 +12,19 @@ namespace dbuddy {
     class Ui {
     public:
         Ui(Fonts * fonts, Styles * styles) : fonts(fonts), styles(styles) {}
+        ~Ui() {
+            delete fonts;
+            delete styles;
+
+            auto it = tasks->begin();
+            while (it != tasks->end()) {
+                delete *it++;
+            }
+        }
 
         void add_widget(widget_t, Widget *);
+        lv_task_t * create_task(lv_task_cb_t, uint32_t period, lv_task_prio_t prio = LV_TASK_PRIO_MID);
+        void * delete_task(lv_task_t * task);
         Widget * get_widget(widget_t) const;
         bool has_widget(widget_t) const;
 
@@ -21,6 +33,8 @@ namespace dbuddy {
     private:
         Fonts * fonts;
         Styles * styles;
+
+        std::vector<lv_task_t *> * tasks = new std::vector<lv_task_t *>;
 
         std::map<widget_t, Widget *> widgets;
     };
--- src/ui.cpp
+++ src/ui.cpp
@@ -9,6 +9,22 @@ void Ui::add_widget(widget_t w, Widget * widget) {
     }
 }
 
+lv_task_t * Ui::create_task(lv_task_cb_t task_xcb, uint32_t period, lv_task_prio_t prio) {
+    lv_task_t * task = lv_task_create(task_xcb, period, prio, this);
+    tasks->push_back(task);
+    return task;
+}
+
+void * Ui::delete_task(lv_task_t * task) {
+    auto it = tasks->begin();
+    while (it != tasks->end()) {
+        if (*it == task) {
+            tasks->erase(it);
+            break;
+        }
+    }
+}
+
 Widget * Ui::get_widget(widget_t widget) const {
     return widgets.at(widget);
 }
