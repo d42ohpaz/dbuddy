170f93785b7c27214bce5873addfcf7769490888
Ken Stanley <dohpaz@gmail.com>
2021-02-07 00:21:51 -0500

    Simplify styles for time_label

--- src/widget-time-label.cpp
+++ src/widget-time-label.cpp
@@ -7,9 +7,10 @@ void TimeLabel::init() {
 
     set_self(lv_label_create(time_container->get_self(), nullptr));
 
-    lv_label_set_text(get_self(), "--:--");
-    lv_obj_set_auto_realign(get_self(), true);
-    lv_obj_align(get_self(), nullptr, LV_ALIGN_IN_LEFT_MID, DEFAULT_PADDING / 2, 0);
+    set_pos(DEFAULT_PADDING / 2, DEFAULT_PADDING / 2);
+
+    lv_label_set_text(get_self(), "");
+    lv_obj_set_auto_realign(get_self(), false);
 
     add_style(LV_LABEL_PART_MAIN, get_ui()->get_fonts()->get_regular_small(LV_STATE_DEFAULT));
     add_style(LV_LABEL_PART_MAIN, get_ui()->get_styles()->get_text_color_white(LV_STATE_DEFAULT));
