50f207f9556578139c74724ff87a544948ab3b43
Ken Stanley <dohpaz@gmail.com>
2021-02-24 07:51:22 -0500

    Inline the callbacks into lambdas.

--- src/hal.cpp
+++ src/hal.cpp
@@ -4,22 +4,6 @@
 
 using namespace dbuddy;
 
-/**
- * The c-callback for flushing our visual data to our display driver.
- */
-extern "C" void display_flush_callback(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color) {
-    auto * instance = (Hal *)drv->user_data;
-    instance->display_flush(drv, area, color);
-}
-
-/**
- * The c-callback for reading our input data from the user.
- */
-extern "C" bool input_read_callback(lv_indev_drv_t * drv, lv_indev_data_t * data) {
-    auto * instance = (Hal *)drv->user_data;
-    return instance->input_read(drv, data);
-}
-
 void Hal::run(bool use_dbl_buff, lv_indev_type_t type) {
     initializeDisplay(use_dbl_buff);
     initializeInputDevice(type);
@@ -37,7 +21,11 @@ void Hal::initializeDisplay(bool use_dbl_buff) {
     // Make the instance available for our c-callback above.
     display_driver->user_data = this;
 
-    display_driver->flush_cb = &display_flush_callback;
+    display_driver->flush_cb = [](lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color) {
+        auto * instance = (Hal *)drv->user_data;
+        instance->display_flush(drv, area, color);
+    };
+
     display_driver->buffer = display_buffer;
 
     if (!lv_disp_drv_register(display_driver)) {
@@ -52,7 +40,10 @@ void Hal::initializeInputDevice(lv_indev_type_t type) {
     input_device_driver->user_data = this;
 
     input_device_driver->type = type;
-    input_device_driver->read_cb = &input_read_callback;
+    input_device_driver->read_cb = [](lv_indev_drv_t * drv, lv_indev_data_t * data) {
+        auto * instance = (Hal *)drv->user_data;
+        return instance->input_read(drv, data);
+    };
 
     if (!lv_indev_drv_register(input_device_driver)) {
         throw std::runtime_error("Unable to register the input driver");
